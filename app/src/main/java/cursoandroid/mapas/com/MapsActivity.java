dde1081e51d9a82267b92a45b4e830a2e1da276b
maellson <maellson@hotmail.com>
2019-02-10 17:51:09 -0300

    Projeto com adicao de pontos, linhas, circulo e poligono finalizado na aula 5

--- app/src/main/java/cursoandroid/mapas/com/MapsActivity.java
+++ app/src/main/java/cursoandroid/mapas/com/MapsActivity.java
@@ -1,5 +1,6 @@
 package cursoandroid.mapas.com;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -10,8 +11,11 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -42,10 +46,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         mMap.setMapType(1);
 
-        // Add a marker na prefeitura de campina and move the camera
-        LatLng prefeitura = new LatLng(-7.2192037, -35.8828192);
+        // Add um marcador na prefeitura de campina and move the camera
+        final LatLng prefeitura = new LatLng(-7.2192037, -35.8828192);
         //-7.2192037, -35.8828192
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+        /*
+        //Construcao dos circulos
+        CircleOptions circulo = new CircleOptions();
+        circulo.center(prefeitura);
+        circulo.radius(50);//definido em metros
+        circulo.strokeWidth(6);//Bord do circulo
+        circulo.strokeColor(Color.argb(128,255,69,0));//Cor da Borda
+        circulo.fillColor(Color.argb(90,255,165,0));
+
+        mMap.addCircle(circulo);
+        */
+
+         /*
+
+        */
+
+         /*
+        //Adicionando um poligono
+        PolygonOptions poligono = new PolygonOptions();
+
+        poligono.add(new LatLng(-7.219126, -35.883137));//1
+        poligono.add(new LatLng(-7.219012, -35.882710));//4
+        poligono.add(new LatLng(-7.219337, -35.882570));//2
+        poligono.add(new LatLng(-7.219403, -35.883067));//3
+        poligono.strokeWidth(6);//Bord do circulo
+        poligono.strokeColor(Color.argb(128,255,69,0));//Cor da Borda
+        poligono.fillColor(Color.argb(90,255,165,0));
+
+
+
+        mMap.addPolygon(poligono);
+        */
+
+        //setando no mapa um clique
+       /* mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
 
@@ -59,7 +98,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         new MarkerOptions()
                                 .position(latLng)
                                 .title("Local")
-                                .snippet("Lat: "+latLng.latitude+ " Long: "+latLng.longitude)
+                                .snippet("Descrição do que foi Adicionado")
 
                                 .icon(
                                         BitmapDescriptorFactory.fromResource(R.drawable.icone_loja)
@@ -68,8 +107,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 );
 
             }
-        });
+        }); */
 
+       //Setando clique longo
+
+        /*
         mMap.setOnMapLongClickListener(
                 new GoogleMap.OnMapLongClickListener() {
                     @Override
@@ -95,15 +137,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     }
                 }
 
-        );
+        ); */
 
         mMap.addMarker(
                 new MarkerOptions()
                         .position(prefeitura)
                         .title("Prefeitura")
-                       /* .icon(
-                               BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)
-                       )*/
                         .icon(
                                 BitmapDescriptorFactory.fromResource(R.drawable.carro)
                         )
@@ -113,6 +152,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 CameraUpdateFactory.newLatLngZoom(prefeitura,18)
         );
 
+
+    //Adicionando linhas
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                Double latitude = latLng.latitude;
+                Double longitude = latLng.longitude;
+
+                PolylineOptions polyline = new PolylineOptions();
+                polyline.add(prefeitura);
+                polyline.add(latLng);
+                polyline.color(Color.BLUE);
+                polyline.width(20);
+
+                mMap.addPolyline(polyline);
+
+                mMap.addMarker(
+                        new MarkerOptions()
+                                .position(latLng)
+                                .title("Local")
+                                .snippet("Descrição do que foi Adicionado")
+
+                                .icon(
+                                        BitmapDescriptorFactory.fromResource(R.drawable.icone_loja)
+                                )
+
+                );
+
+            }
+        });
 
     }
 }
